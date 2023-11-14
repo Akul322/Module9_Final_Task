b03c881a15114b6b2a57265a80492406f832b492
Akul <artiluckashov0000@gmail.com>
2023-11-14 13:39:57 +0300

    Добавьте файлы проекта.

---
+++ Delegate_Final_Task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Final_Task
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exception = new Exception[5];
+            exception[0] = new OverflowException();
+            exception[1] = new FormatException();
+            exception[2] = new ArgumentException();
+            exception[3] = new RankException();
+            exception[4] = new MyException();
+            foreach (Exception ex in exception)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception E)
+                {
+                    Console.WriteLine(E.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
